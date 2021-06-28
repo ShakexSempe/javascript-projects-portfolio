91d136646e27bb50898c382e6dbf1456039bba99
Ray-Shakex <bshakex@gmail.com>
2021-06-28 13:26:30 +0200

    main.js setup

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const projects = [
+    {
+        id: 1,
+        title: "color flipper",
+        link: "https://shakexsempe.github.io/color-flipper/",
+        img: "./img/color-flip.bmp",
+    },
+    {
+        id: 2,
+        title: "sidebar",
+        link: "https://shakexsempe.github.io/sidebar-munch-up/",
+        img: "./img/munch.bmp",
+    },
+    {
+        id: 3,
+        title: "shopping list/",
+        link: "https://shakexsempe.github.io/JS-shopping-list/",
+        img: "./img/grocery.bmp",
+    },
+    {
+        id: 4,
+        title: "countdown timer",
+        link: "https://shakexsempe.github.io/countdown-timer/",
+        img: "./img/countdown.bmp",
+    },
+    {
+        id: 5,
+        title: "video landing page",
+        link: "https://shakexsempe.github.io/video-landing-page/",
+        img: "./img/video.bmp",
+    },
+    {
+        id: 6,
+        title: "menu",
+        link: "https://shakexsempe.github.io/menu-js/",
+        img: "./img/menu.bmp",
+    },
+];
